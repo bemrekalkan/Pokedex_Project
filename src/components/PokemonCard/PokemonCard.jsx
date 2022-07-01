2fa9b73fabeae5ce1b7e23c2e5437834d232a9ff
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-01 18:58:52 +0300

    PokemonCard.jsx

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -6,6 +6,8 @@ import PokemonList from "../pokemonList/PokemonList";
 const PokemonCard = () => {
   const [pokemon, setPokemon] = useState("");
 
+  const [pokemonName, setPokemonName] = useState("");
+
   useEffect(() => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154")
@@ -13,7 +15,23 @@ const PokemonCard = () => {
       .catch((err) => console.log(err));
   }, []);
 
-  return <></>;
+  const handleChange = (e) => {
+    setPokemonName(e.target.value);
+  };
+
+  console.log(pokemonName);
+
+  return (
+    <div className="page">
+      <div>
+        <div className="header">
+          {" "}
+          <label htmlFor="header">POKEMON</label>
+        </div>
+        <input type="text" id="header" onChange={handleChange} />
+      </div>
+    </div>
+  );
 };
 
 export default PokemonCard;
