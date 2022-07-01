76c0fcfa69a797925fc554400577920e8673e619
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-01 18:51:14 +0300

    get-then-catch done

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -4,16 +4,13 @@ import axios from "axios";
 import PokemonList from "../pokemonList/PokemonList";
 
 const PokemonCard = () => {
-  const [pokemon, setPokemon] = useState(
-    "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154"
-  );
+  const [pokemon, setPokemon] = useState("");
 
   useEffect(() => {
-    axios.get("");
-
-    return () => {
-      second;
-    };
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154")
+      .then((response) => setPokemon(response.data.results))
+      .catch((err) => console.log(err));
   }, []);
 
   return <></>;
