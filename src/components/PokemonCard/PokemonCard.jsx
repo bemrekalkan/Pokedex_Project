19a76b2865280b6d3e0d8478331937c729b41383
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-01 18:43:31 +0300

    api added with axios

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,9 +1,21 @@
 // import { useEffect } from "react";
-// import { useState } from "react";
-// import axios from "axios";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import PokemonList from "../pokemonList/PokemonList";
 
 const PokemonCard = () => {
+  const [pokemon, setPokemon] = useState(
+    "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154"
+  );
+
+  useEffect(() => {
+    axios.get("");
+
+    return () => {
+      second;
+    };
+  }, []);
+
   return <></>;
 };
 
